58598fd2219a7ea3af69e9d24ad8e4d0f02815f7
Alex Ledger <a.led1027@gmail.com>
2024-08-17 14:35:07 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import '../styles/main.scss';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Counter from '$lib/components/Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="flow mx-auto text-center">
+	<span>
+		<picture>
+			<source srcset={welcome} type="image/webp" />
+			<img src={welcome_fallback} alt="Welcome" />
+		</picture>
+	</span>
+	<h1 class="heading-1 mx-auto">Welcome</h1>
+	<div class="flex-group mx-auto">
+		<Counter />
+	</div>
+</section>
