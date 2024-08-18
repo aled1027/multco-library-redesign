fa81b92b0e76a0aac9a6b7acb9b7aeebc35456c9
Alex Ledger <alex@playgroundlabs.io>
2024-08-18 10:04:29 -0700

    Add header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,12 +2,20 @@
 </script>
 
 <header class="site-header">
-	<ul>
-		<li>
-			<a href="/">Home</a>
-		</li>
-		<li>
-			<a href="/events">Events</a>
-		</li>
-	</ul>
+	<div class="site-header__inner">
+		<div class="title-container">
+			<a href="/">Multco Library Events</a>
+			<span class="subtitle">An Unofficial Resource of Multnomah County Library Events</span>
+		</div>
+		<button class="menu-button">Menu</button>
+
+		<ul class="visually-hidden">
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/events">Events</a>
+			</li>
+		</ul>
+	</div>
 </header>
