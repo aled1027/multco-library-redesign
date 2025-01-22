bbf72420d392e5a6f8cd6aa50e2234ef4b7193cc
Alex Ledger <alex@playgroundlabs.io>
2025-01-22 10:55:40 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,8 +53,8 @@
 	const defaultFilterValues = {
 		locations: [],
 		searchQuery: '',
-		startDate: '2024-10-13',
-		endDate: formatDate(addDaysToDate(new Date(), 60))
+		startDate: '2024-08-14',
+		endDate: '2024-10-14'
 	};
 	const filter = writable<Filter>(_.clone(defaultFilterValues));
 
